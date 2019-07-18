fecd44dabcbb66f7f646423dedde51cd52c11bce
Saravanan Rajendran <sarrajen@publicisgroupe.net>
2019-07-18 09:07:00 +0530

    removed all files

--- Sonar/jpath.bat
+++
@@ -1,5 +0,0 @@
-set path=C:\Users\sarrajen\Desktop\sonar-scanner-4.0.0.1744-windows\bin;.;%path%
-set path=C:\Users\sarrajen\Desktop\sonarqube-7.9\bin\windows-x86-64;.;%path%
-set path=C:\Program Files\Java\jdk-12.0.1\bin;.;%path%
-set classpath=.;%classpath%
-start
